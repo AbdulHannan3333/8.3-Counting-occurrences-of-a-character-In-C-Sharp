e17ec67b563c09ead0ee59ec9c8d7ff1f52af68e
AbdulHannan3333 <hannan67212@outlook.com>
2024-06-16 04:10:48 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8._3_Counting_occurrences_of_a_character
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "Programming";
+            char target = 'g';
+            int count = 0;
+            foreach (var item in input)
+            {
+                if (item == target) 
+                {
+                count++;
+                }
+            }
+            Console.WriteLine($"Occurrance of '{target}' in '{input}': '{count}'");
+        }
+    }
+}
